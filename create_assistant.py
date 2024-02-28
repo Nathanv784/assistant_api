c4c3d30f33fd21dc6a15c1a7ab38178af922787d
nathanv784 <nathanvirushabadoss@bitcot.com>
2024-02-28 10:47:40 +0530

    updated

--- create_assistant.py
+++ create_assistant.py
@@ -7,26 +7,26 @@ def create_assistant():
     client = OpenAI(api_key=api_key)
 
     # List of filenames to upload
-    # file_names = ["state_of_the_union.txt", "sachin.txt"]
+    file_names = ["state_of_the_union.txt", "sachin.txt"]
 
-    # # Loop through each file name and upload it to OpenAI
-    # for file_name in file_names:
-    #     with open(file_name, 'rb') as file:
-    #         uploaded_file = client.files.create(file=file, purpose='assistants')
+    # Loop through each file name and upload it to OpenAI
+    for file_name in file_names:
+        with open(file_name, 'rb') as file:
+            uploaded_file = client.files.create(file=file, purpose='assistants')
 
-    #     # Add the uploaded file to the existing assistant
-    #     client.beta.assistants.files.create(assistant_id="asst_KPScZO2boVuj51jEv0mw2Dxy", file_id=uploaded_file.id)
+        # Add the uploaded file to the existing assistant
+        client.beta.assistants.files.create(assistant_id="asst_KPScZO2boVuj51jEv0mw2Dxy", file_id=uploaded_file.id)
 
-    # # Create an assistant
-    # assistant = client.beta.assistants.create(
-    #     name="ContentQueryAssistant",
-    #     instructions="ContentQueryAssistant is designed to assist users with queries related to specific content domains. When interacting with users, always begin with a warm greeting to create a friendly atmosphere. The primary goal is to provide helpful responses to user inquiries within the designated content area. It's important to remain focused on addressing queries within the defined domain and to politely decline requests outside of this scope. User feedback is valuable for continual improvement, so attentively listen to and consider any suggestions or corrections provided. Conclude interactions with a courteous closing remark, inviting users to reach out for further assistance if needed.",
-    #     tools=[{"type": "retrieval"}],
-    #     model="gpt-4-1106-preview",
-    #     file_ids=[file.id]  
-    # )
+    # Create an assistant
+    assistant = client.beta.assistants.create(
+        name="ContentQueryAssistant",
+        instructions="ContentQueryAssistant is designed to assist users with queries related to specific content domains. When interacting with users, always begin with a warm greeting to create a friendly atmosphere. The primary goal is to provide helpful responses to user inquiries within the designated content area. It's important to remain focused on addressing queries within the defined domain and to politely decline requests outside of this scope. User feedback is valuable for continual improvement, so attentively listen to and consider any suggestions or corrections provided. Conclude interactions with a courteous closing remark, inviting users to reach out for further assistance if needed.",
+        tools=[{"type": "retrieval"}],
+        model="gpt-4-1106-preview",
+        file_ids=[file.id]  
+    )
 
-    # print(assistant)
+    print(assistant)
 
 if __name__ == "__main__":
     create_assistant()
--- update_assistant.py
+++ update_assistant.py
@@ -5,13 +5,13 @@ def update_assistant():
     api_key = os.getenv('api_key')
     client = OpenAI(api_key=api_key)
 
-    # Update assistant
-    # my_updated_assistant = client.beta.assistants.update(
-    #     "asst_47YkPu5MhRmUd9zVtYLh5Ldz",
-    #     instructions="You are an ContentQueryAssistant, and you have access to files to answer user questions. When interacting with users, always begin with a warm greeting to create a friendly atmosphere and Always response with information from either of the files."
-    # )
+    #Update assistant
+    my_updated_assistant = client.beta.assistants.update(
+        "asst_47YkPu5MhRmUd9zVtYLh5Ldz",
+        instructions="You are an ContentQueryAssistant, and you have access to files to answer user questions. When interacting with users, always begin with a warm greeting to create a friendly atmosphere and Always response with information from either of the files."
+    )
 
-    # print(my_updated_assistant)
+    print(my_updated_assistant)
 
 if __name__ == "__main__":
     update_assistant()
